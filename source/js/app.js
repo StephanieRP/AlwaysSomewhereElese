6ebe86030802c37af991fe10c638447d685edb0b
Stephanie <stephaniepatton@stephanies-mbp.attlocal.net>
2018-04-26 22:02:49 -0500

    finished about section

---
+++ source/js/app.js
@@ -0,0 +1,11 @@
+// Mobile View Slide out menu
+
+function openSideMenu() {
+  document.getElementById('mobile-nav').style.width = '100%';
+  document.body.style.backgroundColor = "rgba(0,146,193.4)";
+}
+
+function closeSideMenu() {
+  document.getElementById('mobile-nav').style.width = '0';
+  document.body.style.backgroundColor = "rgba(0,146,193,0)";
+}
