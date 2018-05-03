778c68b552ca07c40552124cfe948d63774e9334
Stephanie <stephaniepatton@stephanies-mbp.attlocal.net>
2018-05-03 15:37:17 -0500

    fixed category accordion menu

--- source/js/app.js
+++ source/js/app.js
@@ -21,74 +21,19 @@ function openNav() {
 
 // Categories Dropdown
 
-var cate = document.querySelector(".category__title")
-var dropButton = 
-document.querySelector(".dropdown-icon");
-var dropdownList = document.querySelector(".dropdown-list");
+var dropAcc = document.getElementsByClassName("category__title");
 
-function dropCate() {
-  cate.addEventListener("click", function(){
-    dropButton.classList.toggle("dropdown-open"),
-    dropdownList.classList.toggle("dropdown-active")
-  });
-  // if(!dropdownList.hasClass('show')) {
-  //   menu.classList.add('show');
-  //   menu.classList.remove('hide');
-  //   arrow.classList.add('open');
-  //   arrow.classList.remove('close');
-  // }
-  // else {
-  //   menu.classList.remove('show');
-  //   menu.classList.add('hide');
-  //   arrow.classList.remove('open');
-  //   arrow.classList.add('close');
-  // }
-}
+for (var i = 0; i < dropAcc.length; i++) {
+    dropAcc[i].addEventListener("click", function() {
+      this.classList.toggle("display");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}    
+
 
 openNav();
-dropCate();
-
-
-
-
-// function openSideMenu() {
-//   document.getElementById('mobile-nav').style.width = '100%';
-//   document.body.style.backgroundColor = "rgba(0,146,193.4)";
-// }
-
-// function closeSideMenu() {
-//   document.getElementById('mobile-nav').style.width = '0';
-//   document.body.style.backgroundColor = "rgba(0,146,193,0)";
-// }
-
-
-
-//// Dropdown for Categories
-// var dropdown = document.querySelector('.dropdown-list');
-// var dropdownArray = Array.prototype.slice.call(dropdown,0);
-// dropdownArray.forEach(function(el){
-//   var button = el.querySelector('i.dropdown-icon'),
-// 			menu = el.querySelector('.dropdown-list'),
-// 			arrow = button.querySelector('i.icon-arrow');
-
-// 	button.onclick = function(event) {
-// 		if(!menu.hasClass('dropdown-active')) {
-// 			menu.classList.add('dropdown-active');
-// 			menu.classList.remove('dropdown-inactive');
-// 			arrow.classList.add('dropdown-open');
-// 			arrow.classList.remove('dropdown-close');
-// 			event.preventDefault();
-// 		}
-// 		else {
-// 			menu.classList.remove('dropdown-active');
-// 			menu.classList.add('dropdown-inactive');
-// 			arrow.classList.remove('dropdown-open');
-// 			arrow.classList.add('dropdown-close');
-// 			event.preventDefault();
-// 		}
-// 	};
-// })
-
-// Element.prototype.hasClass = function(className) {
-//     return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
-// };
